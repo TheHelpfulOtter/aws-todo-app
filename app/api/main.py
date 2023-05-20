4c8ccc323de95c1e0943b7354d739ef095b8144a
Daniel Kuan <28009534+TheHelpfulOtter@users.noreply.github.com>
2023-05-20 13:53:50 +0300

    [wIP] Add new route to API for testing

--- app/api/main.py
+++ app/api/main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"status": 200, "message": "If you can see this, hi!"}
+
+@app.get("/test")
+async def test_route():
+    return {"message": "Why, hello there."}
