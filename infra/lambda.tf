af0957fd17fb64ae74e026610e4f51ca307b4f4f
Daniel Kuan <28009534+TheHelpfulOtter@users.noreply.github.com>
2023-06-09 07:35:59 +0300

    Tidy up lamba roles for readability

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -2,11 +2,6 @@
 # Lambda Function (LF) to run container from ECR
 ################################################################################################
 
-resource "aws_iam_role_policy_attachment" "todo_lambda_policy_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-  role       = aws_iam_role.todo_lambda_role.name
-}
-
 resource "aws_lambda_function" "todo_lambda" {
   function_name = "todo-lambda"
   role          = aws_iam_role.todo_lambda_role.arn
@@ -42,4 +37,9 @@ data "aws_iam_policy_document" "assume_role_lambda" {
 resource "aws_iam_role" "todo_lambda_role" {
   name               = "todo-lambda-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role_lambda.json
+}
+
+resource "aws_iam_role_policy_attachment" "todo_lambda_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.todo_lambda_role.name
 }
