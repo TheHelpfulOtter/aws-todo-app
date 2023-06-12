148b8c1f1f0ce1a72e1465f14bb9c87bbbea70ef
Daniel Kuan <dkuan0808@outlook.com>
2023-06-12 12:29:16 +0300

    [TEST] List tasks not getting user id

--- app/api/main.py
+++ app/api/main.py
@@ -1,21 +1,23 @@
+import os
+import boto3
+import time
+
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from mangum import Mangum
 from pydantic import BaseModel
 from typing import Optional
 from uuid import uuid4
-
-import os
-import boto3
-import time
+from boto3.dynamodb.conditions import Key
 
 app = FastAPI(root_path="/dev/")
 handler = Mangum(app)
 
-stage = os.environ.get("STAGE", "dev")
-
-
+################################################################################
 # Models
+################################################################################
+
+
 class PutTaskRequest(BaseModel):
     content: str  # Description of todo item.
     user_id: Optional[str] = None
@@ -23,38 +25,121 @@ class PutTaskRequest(BaseModel):
     completed: bool = False
 
 
+################################################################################
 # API Endpoints
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html
+################################################################################
+
+table_name = "Tasks"
+boto3_client = boto3.client("dynamodb")
+
+
+# Root
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
+# Create task
 @app.put("/create-task")
 async def create_task(put_task_request: PutTaskRequest):
+    task_id = f"task_{uuid4().hex}"
     time_created = int(time.time())
+    ttl = str(time_created + 86400)
 
     item = {
-        "user_id": put_task_request.user_id,
-        "content": put_task_request.content,
-        "completed": False,  # Task should be incomplete by default.
-        "time_created": time_created,
-        "task_id": f"task_{uuid4().hex}",  # hex creates a string representation of the UUID without hyphens or any other separators.
-        "ttl": int(time_created + 86400),  # Task will expire and delete after 24 hours.
+        "user_id": {"S": put_task_request.user_id},
+        "content": {"S": put_task_request.content},
+        "completed": {
+            "BOOL": put_task_request.completed
+        },  # Task should be incomplete by default.
+        "time_created": {"N": str(time_created)},
+        "task_id": {
+            "S": task_id
+        },  # hex creates a string representation of the UUID without hyphens or any other separators.
+        "ttl": {"N": ttl},  # Task will expire and delete after 24 hours.
     }
 
     # Put the task into the table.
-    table = _get_table()
-    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb/client/put_item.html
-    table.put_item(Item=item)
+    client = boto3_client
+    response = client.put_item(TableName=table_name, Item=item)
 
     # Return value for use in frontend.
-    return {"task": item}
+    return {"created task": item}
 
 
-def _get_table():
-    table_name = os.environ.get("DYNAMODB_TABLE_NAME")  # Gets table name from output.tf
-    return boto3.resource("dynamodb").Table(table_name)
+# Update task
+@app.put("/update-task")
+async def update_task(put_task_request: PutTaskRequest):
+    # Update the task in the table.
+    client = boto3_client
+    client.update_item(
+        TableName=table_name,
+        Key={"task_id": {"S": put_task_request.task_id}},
+        UpdateExpression="SET content = :content, completed = :completed",
+        ExpressionAttributeValues={
+            ":content": put_task_request.content,
+            ":completed": put_task_request.completed,
+        },
+        ReturnValues="ALL_NEW",
+    )
+    return {"updated_task_id": put_task_request.task_id}
 
 
+# Delete specific task via task id
+@app.delete("/delete-task/{task_id}")
+async def delete_task(task_id: str):
+    # Delete the task from the table.
+    client = boto3_client
+    client.delete_item(TableName=table_name, Key={"task_id": {"S": task_id}})
+    return {"deleted_task_id": task_id}
+
+
+# Get specific task from task id
+@app.get("/get-task/{task_id}")
+async def get_task(task_id: str):
+    # Get the task from the table.
+    client = boto3_client
+    response = client.get_item(TableName=table_name, Key={"task_id": {"S": task_id}})
+
+    item = response.get("Item")
+
+    if not item:
+        raise HTTPException(status_code=404, detail=f"Task {task_id} not found")
+
+    return item
+
+
+# List tasks associated with user id
+@app.get("/list-tasks/{user_id}")
+async def list_tasks(user_id: str):
+    # List the top N tasks from the table, using the user index.
+    client = boto3_client
+    response = client.query(
+        TableName=table_name,
+        IndexName="UserIndex",
+        KeyConditionExpression="user_id = :user_id",
+        ExpressionAttributeValues={":user_id": {"S": str(user_id)}},
+        ScanIndexForward=False,
+        Limit=10,
+    )
+    tasks = response["Items"]
+
+    return tasks
+
+
+# List Tasks via User ID
+# @app.get("/list-tasks/{user_id}")
+# def get_user_tasks(user_id: str):
+#     client = boto3_client
+
+#     query = f"SELECT * FROM Tasks WHERE user_id = '{user_id}'"
+#     response = client.execute_statement(Statement=query)
+
+#     items = response["Items"]
+#     return items
+
+
+# Start Uvicorn
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9000)
